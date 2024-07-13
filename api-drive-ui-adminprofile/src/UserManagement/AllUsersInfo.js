de8bd496056ee2f4e9d69a2ca89ff0f660d6ea57
Triveni-API-Drive <gangadhar.reddy@canopusgbs.com>
2024-07-13 09:46:24 +0530

    Delete api-drive-ui-adminprofile directory

--- api-drive-ui-adminprofile/src/UserManagement/AllUsersInfo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserInfo = () => {
-  return <h2>All User Info Page</h2>;
-};
-
-export default UserInfo;
--- api-drive-ui-adminprofile/src/UserManagement/EditUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const EditUser = () => {
-  return <h2>Edit User Page</h2>;
-};
-
-export default EditUser;
